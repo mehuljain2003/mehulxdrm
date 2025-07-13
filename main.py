9356edda4dfcb0341704aaee50d9bacfecbaf7f8
mehuljain2003 <85852792+mehuljain2003@users.noreply.github.com>
2025-07-13 10:33:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,12 +63,12 @@ photozip = 'https://envs.sh/cD_.jpg'
 
 
 # Inline keyboard for start command
-BUTTONSCONTACT = InlineKeyboardMarkup([[InlineKeyboardButton(text="📞 Contact", url="https://t.me/saini_contact_bot")]])
+BUTTONSCONTACT = InlineKeyboardMarkup([[InlineKeyboardButton(text="📞 Contact", url="https://t.me/jattprab")]])
 keyboard = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="🛠️ Help", url="https://t.me/+3k-1zcJxINYwNGZl"),
-            InlineKeyboardButton(text="🛠️ Repo", url="https://github.com/nikhilsainiop/saini-txt-direct"),
+            InlineKeyboardButton(text="🛠️ Help", url="https://t.me/jattprab"),
+            InlineKeyboardButton(text="🛠️ Repo", url="https://github.com/-direct"),
         ],
     ]
 )
